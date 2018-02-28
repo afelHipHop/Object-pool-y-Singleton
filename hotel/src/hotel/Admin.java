e29126b70f22525222d0c6129ffc79468d34752a
afelHipHop <afelrh@hotmail.com>
2018-02-28 13:22:39 -0500

    Update Admin.java

--- hotel/src/hotel/Admin.java
+++ hotel/src/hotel/Admin.java
@@ -3,22 +3,31 @@ package hotel;
 
 public class Admin {
     
-    private Habitacion[] habitaciones= new Habitacion[10];
-    Hotel h;
-    
-    public void verificarDisponibilidad(){
-        h = Hotel.getInstance();
-        for(int i=0; i<4; i++){
-            System.out.println("Habitacion: "+habitaciones[i].getNumero()+" Disponible: "+habitaciones[i].isDisponible());
-        }
-    }
-    
-    public void asignarHabitacion(){
-        h.getHabitacion();
-    }
-    
-    public void registrarSalida(int n){
-        h.desocuparHabitacion(habitaciones[n-1]);
-    }
+   Habitacion habitacion1;
+   Habitacion habitacion2;
+   Habitacion habitacion3;
+   Habitacion habitacion4;
+   Habitacion habitacion5;
+   
+   Hotel hotel;
+   
+   public void asignacion(){
+       hotel = Hotel.getInstance();
+       
+       habitacion1 = hotel.getHabitacion();
+       System.out.println(habitacion1.getNumero());
+       habitacion2 = hotel.getHabitacion();
+       System.out.println(habitacion2.getNumero());
+       habitacion3 = hotel.getHabitacion();
+       System.out.println(habitacion3.getNumero());
+       hotel.desocuparHabitacion(habitacion1);
+       habitacion1 = hotel.getHabitacion();
+       System.out.println(habitacion1.getNumero());
+       habitacion4 = hotel.getHabitacion();
+       System.out.println(habitacion4.getNumero());
+       hotel.desocuparHabitacion(habitacion3);
+       habitacion3 = hotel.getHabitacion();
+       System.out.println(habitacion3.getNumero());
+   }
     
 }
