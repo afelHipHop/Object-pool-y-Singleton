37b2b5b4dcee6db763578d95dce90802ed02ccbd
afelHipHop <afelrh@hotmail.com>
2018-02-28 13:23:39 -0500

    Update Main.java

--- hotel/src/hotel/Main.java
+++ hotel/src/hotel/Main.java
@@ -15,29 +15,7 @@ public class Main {
 
     public static void main(String[] args) {
         Admin a = new Admin();
-        a.verificarDisponibilidad();
-        System.out.println("¿Asignar habitacion?");
-        Scanner s = new Scanner(System.in);
-        String text = s.nextLine();
-        if (text.equals("Si"))
-            a.asignarHabitacion();
-        else{
-            System.out.println("¿Liberar habitacion?");
-            s = new Scanner(System.in);
-            text = s.nextLine();
-            if (text.equals("Si")){
-                System.out.println("¿que habitacion desea liberar?");
-                s = new Scanner(System.in);
-                text = s.nextLine();
-                if(Integer.parseInt(text)>0 && Integer.parseInt(text)<12)
-                    a.registrarSalida(Integer.parseInt(text));
-                else
-                    System.out.println("valor invalido, intentelo mas tarde");
-            }
-            else
-                System.out.println("vuelva pronto");
-            
-        }
+       a.asignacion();
         
     }
     
